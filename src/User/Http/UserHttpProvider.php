8debd9efb2debfd02aae7205e8c5497ce4aaad3d
Simon Holloway <simon.holloway@givesmart.com>
2017-10-18 21:05:39 +0100

    kick off the domain base

---
+++ src/User/Http/UserHttpProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: simon
+ * Date: 18/10/17
+ * Time: 19:38
+ */
+class UserHttpProvider
+{
+
+}
---
+++ src/User/UserProvider.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\User\Console;
+
+class UserConsoleProvider
+{
+
+}
